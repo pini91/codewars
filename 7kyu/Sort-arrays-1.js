b1dcc23fd53d909b57a6449328bd16694edb6a60
pini91 <brenda.loncaric@gmail.com>
2023-12-16 12:08:34 -0600

    Adding new coding challenge.

---
+++ 7kyu/Sort-arrays-1.js
@@ -0,0 +1,10 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    return names.sort()
+}
+
+console.log(sortme(['one', 'two', 'three' ]))
