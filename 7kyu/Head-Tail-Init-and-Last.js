45885a21cb99badd16660132de7570c33ff1ac09
pini91 <brenda.loncaric@gmail.com>
2023-12-20 12:05:06 -0600

    Adding new coding challenge.

---
+++ 7kyu/Head-Tail-Init-and-Last.js
@@ -0,0 +1,20 @@
+// Haskell has some useful functions for dealing with lists:
+
+// $ ghci
+// GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+// λ head [1,2,3,4,5]
+// 1
+// λ tail [1,2,3,4,5]
+// [2,3,4,5]
+// λ init [1,2,3,4,5]
+// [1,2,3,4]
+// λ last [1,2,3,4,5]
+// 5
+// Your job is to implement these functions in your given language. Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
+
+const head =(x) => x[0]
+const tail =(x) => x.slice(1)
+const init =(x) => x.slice(0,x.length -1)
+const last = (x) => x[x.length -1]
+
+console.log(last([7,2]))
