7cca8f1bae7e38327fac218dfa1024f71bf376f7
pini91 <brenda.loncaric@gmail.com>
2024-03-10 13:48:45 -0500

    Adding new coding challenge.

---
+++ 7kyu/Sum-of-Minimums.js
@@ -0,0 +1,25 @@
+// Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+// For Example:
+
+// [ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+// , [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+// , [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+// ]
+// So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+// Note: You will always be given a non-empty list containing positive values.
+
+// ENJOY CODING :)
+
+function sumOfMinimums(arr) {
+    let mins = []
+    arr.forEach(element => {
+        mins.push(Math.min(...element))   
+    });
+    return mins.reduce((acc,el) => acc+el)
+
+    //OR eturn arr.reduce((p, c) => p + Math.min(...c), 0);
+}
+
+sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])
