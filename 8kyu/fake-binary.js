181c3fbf2e5e5f5e3a859e0fe7755434fc6a3d5b
pini91 <brenda.loncaric@gmail.com>
2024-04-04 21:10:14 -0500

    Adding new coding challenge.

---
+++ 8kyu/fake-binary.js
@@ -0,0 +1,17 @@
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+function fakeBin(x){
+    result = ''
+    for(let letter of x){
+        if(letter<5){
+            result +='0'
+        }else{
+            result +='1'
+        }
+    }
+    console.log(result)
+}
+
+fakeBin('45385593107843568')
