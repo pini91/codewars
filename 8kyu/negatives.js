e3ea2dbd8950ba5f541664ae54a0ceb8f09d6c51
pini91 <114957700+pini91@users.noreply.github.com>
2024-04-10 19:05:40 -0500

    Add files via upload

---
+++ 8kyu/negatives.js
@@ -0,0 +1,10 @@
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+
+
+function makeNegative(num) {
+    return num < 0 ? num : -num;
+  }
+
