f6df5e245e30e0e01cd9cf1336846b9e67ef0314
pini91 <brenda.loncaric@gmail.com>
2024-05-23 09:57:29 -0500

    Adding coding challenge.

---
+++ 8kyu/boolean-to-string.js
@@ -0,0 +1,7 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+function boolean_to_string(b){
+    return b.toString()
+}
