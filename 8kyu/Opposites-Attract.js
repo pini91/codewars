95fca1d42bbd3058f5826a178800acc0e2dfd3f9
pini91 <brenda.loncaric@gmail.com>
2024-06-09 12:50:17 -0500

    Adding new coding challenge.

--- 8kyu/Opposites-Attract.js
+++ 8kyu/Opposites-Attract.js
@@ -4,13 +4,6 @@
 
 function lovefunc(flower1, flower2){
     return ((flower1%2===0 && flower2%2!==0)|| (flower1%2!==0 && flower2%2===0))
-    // if(flower1%2===0 && flower2%2!==0){
-    //     return true}
-    // // }else if(flower1%3===0 && flower2%2===0){
-    // //     return true
-    // // }else{
-    // //     return false
-    // // }
   }
 
 console.log(lovefunc(0,0))
