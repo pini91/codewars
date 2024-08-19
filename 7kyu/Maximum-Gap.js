8ca4db9e07b629cf10302fcf752f60df33baea6d
pini91 <brenda.loncaric@gmail.com>
2024-08-19 13:18:01 -0500

    Adding new coding challenge.

---
+++ 7kyu/Maximum-Gap.js
@@ -0,0 +1,32 @@
+// Task
+// Given an array/list [] of integers , Find The maximum difference between the successive elements in its sorted form.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives and negatives also zeros_
+
+// Repetition of numbers in the array/list could occur.
+
+// The Maximum Gap is computed Regardless the sign.
+
+// Input >> Output Examples
+// maxGap ({13,10,5,2,9}) ==> return (4)
+// Explanation:
+// The Maximum Gap after sorting the array is 4 , The difference between 9 - 5 = 4 .
+// maxGap ({-3,-27,-4,-2}) ==> return (23)
+
+function maxGap (numbers){
+    let result = []
+
+    let nums = numbers.sort((a,b)=> a-b)
+    for(let i =0; i<nums.length; i++){
+        result.push(nums[i+1]-nums[i]) // This because is in a sorted way if is ascending it would be nums[i]-nums[i+1]
+    }
+    result = result.slice(0, result.length-1)
+    return Math.max(...result)
+   
+    
+}
+
+console.log(maxGap([24,299,131,14,26,25]))
