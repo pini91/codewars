464a700bb161eb5b08d779190a7312c94fbe8b62
pini91 <brenda.loncaric@gmail.com>
2024-08-20 15:09:06 -0500

    Adding new coding challenge.

---
+++ 7kyu/Double-Every-Other.js
@@ -0,0 +1,17 @@
+// Write a function that doubles every second integer in a list, starting from the left.
+
+// Example:
+
+// For input array/list :
+
+// [1,2,3,4]
+// the function should return :
+
+// [1,4,3,8]
+
+
+function doubleEveryOther(a) {
+    return a.map((el,index)=> index%2!==0? el*2 : el )
+  }
+
+console.log(doubleEveryOther([1,2,3,4]))
