9d954b60c4b3ae2b3ad53d48fbe396383f1d30eb
pini91 <brenda.loncaric@gmail.com>
2024-09-07 14:16:21 -0500

    Adding new coding challenge.

---
+++ 7kyu/Partial-Word-Searching.js
@@ -0,0 +1,26 @@
+// Write a method that will search an array of strings for all strings that contain another string, ignoring capitalization. Then return an array of the found strings.
+
+// The method takes two parameters, the query string and the array of strings to search, and returns an array.
+
+// If the string isn't contained in any of the strings in the array, the method returns an array containing a single string: "Empty" (or Nothing in Haskell, or "None" in Python and C)
+
+// Examples
+// If the string to search for is "me", and the array to search is ["home", "milk", "Mercury", "fish"], the method should return ["home", "Mercury"].
+
+
+function wordSearch(query, seq){
+    let result =[]
+    seq.forEach(element => {
+        if(element.toLowerCase().includes(query.toLowerCase())){
+            result.push(element)
+        }
+    });
+    if(result.length===0){
+        return ["Empty"]
+    }else{
+        return result
+    }
+  }
+
+
+  console.log(wordSearch("ab", ["za", "ab", "abc", "zab", "zbc"]))
