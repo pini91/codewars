d23360c49c5d890d08b66dc0a9b10c25da58370e
pini91 <brenda.loncaric@gmail.com>
2024-09-11 16:56:47 -0500

    Adding new coding challenge.

---
+++ 7kyu/Fibonacci-sequence.js
@@ -0,0 +1,19 @@
+// You will be given a function, isFibo, which will take three arguments: a, b and f.
+
+// a is the first term of a Fibonacci sequence, b is the second term of this sequence ( it will always be bigger than or equal to a and they'll be both always positive, so don't worry about pesky input checking ) and f is a number which you'll need to check whether or not is a part of this Fibonacci sequence.
+
+// Good luck, muh fellow coding warrior.
+
+function isFibo(a,b,f) {
+    // while (a < f) {
+    //     let t = a;
+    //     a = b;
+    //     b = t + b;
+    //   }
+    //   return a == f;
+
+     while( a < f ){ [a,b] = [b, a+b] }
+      return a==f
+  }
+
+console.log(isFibo(4,5,23))
