b4d5c49d579dc0d425788772922f7b6ecbf08d5a
pini91 <brenda.loncaric@gmail.com>
2024-10-04 16:12:19 -0400

    Adding new coding challenge.

---
+++ 7kyu/The-averag-length.js
@@ -0,0 +1,20 @@
+// Given an array of strings of the same letter type. Return a new array, which will differ in that the length of each element is equal to the average length of the elements of the previous array.
+
+// A few examples:
+
+// ['u', 'y'] =>  ['u', 'y'] // average length is 1
+// ['aa', 'bbb', 'cccc'] => ['aaa', 'bbb', 'ccc'] // average length is 3
+// ['aa', 'bb', 'ddd', 'eee'] => ['aaa', 'bbb', 'ddd', 'eee'] // average length is 2.5 round up to 3
+// If the average length is not an integer, use Math.round().
+// The input array's length > 1
+
+function averageLength(array) { 
+
+    let avg = Math.round(array.reduce((acc,el)=> acc+= el.length, 0)/array.length)
+
+    let result= array.map(el=> el[0].repeat(avg))
+
+    console.log(result)
+  }
+
+  averageLength(["xxxxxxxxx","hhhhhhhhhh","w","vvvvvvvvvv","gggggggggg","a","ddddddd","ggggg","hhhh"])
