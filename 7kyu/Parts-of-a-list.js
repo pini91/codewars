2ce9c271f7368c7ee9681bf74cf6f2dd8906f0c4
pini91 <brenda.loncaric@gmail.com>
2025-03-11 14:38:08 -0400

    Adding new coding challenge.

---
+++ 7kyu/Parts-of-a-list.js
@@ -0,0 +1,37 @@
+// Write a function partlist that gives all the ways to divide a list (an array) of at least two elements into two non-empty parts.
+
+// Each two non empty parts will be in a pair (or an array for languages without tuples or a structin C - C: see Examples test Cases - )
+// Each part will be in a string
+// Elements of a pair must be in the same order as in the original array.
+// Examples of returns in different languages:
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [["az", "toto picaro zone kiwi"], ["az toto", "picaro zone kiwi"], ["az toto picaro", "zone kiwi"], ["az toto picaro zone", "kiwi"]] 
+// or
+//  a = {"az", "toto", "picaro", "zone", "kiwi"} -->
+// {{"az", "toto picaro zone kiwi"}, {"az toto", "picaro zone kiwi"}, {"az toto picaro", "zone kiwi"}, {"az toto picaro zone", "kiwi"}}
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or 
+// a = [|"az", "toto", "picaro", "zone", "kiwi"|] -->
+// [("az", "toto picaro zone kiwi"), ("az toto", "picaro zone kiwi"), ("az toto picaro", "zone kiwi"), ("az toto picaro zone", "kiwi")]
+// or
+// a = ["az", "toto", "picaro", "zone", "kiwi"] -->
+// "(az, toto picaro zone kiwi)(az toto, picaro zone kiwi)(az toto picaro, zone kiwi)(az toto picaro zone, kiwi)"
+// Note
+// You can see other examples for each language in "Your test cases"       
+
+
+function partlist(arr) {
+    let result = []
+    for(let i=0; i<arr.length-1; i++){
+        if(i===0){
+            result.push(arr.slice(0,i+1).concat(arr.slice(i+1).join(' ')))  
+        }else{
+            result.push(Array.of(arr.slice(0,i+1).join(' ')).concat(arr.slice(i+1).join(' ')))  
+        }                   
+    }
+    console.log(result)
+}
+
+partlist(["I", "wish", "I", "hadn't", "come"])
