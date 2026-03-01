74fa6ec6aaa441c2bc9f2cf7b369370824542ca3
pini91 <brenda.loncaric@gmail.com>
2026-03-01 11:54:11 -0500

    Adding new codewars challenge.

---
+++ 7kyu/Moves-in-squared-strings.js
@@ -0,0 +1,59 @@
+// This kata is the first of a sequence of four about "Squared Strings".
+
+// You are given a string of n lines, each substring being n characters long: For example:
+
+// s = "abcd\nefgh\nijkl\nmnop"
+
+// We will study some transformations of this square of strings.
+
+// Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+// vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+// Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+//  hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+// or printed:
+
+// vertical mirror   |horizontal mirror   
+// abcd --> dcba     |abcd --> mnop 
+// efgh     hgfe     |efgh     ijkl 
+// ijkl     lkji     |ijkl     efgh 
+// mnop     ponm     |mnop     abcd 
+// Task:
+// Write these two functions
+// and
+
+// high-order function oper(fct, s) where
+
+// fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+// Examples:
+// s = "abcd\nefgh\nijkl\nmnop"
+// oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+// oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+// Note:
+// The form of the parameter fct in oper changes according to the language. You can see each form according to the language in "Sample Tests".
+
+// Bash Note:
+// The input strings are separated by , instead of \n. The output strings should be separated by \r instead of \n. See "Sample Tests".
+
+
+function vertMirror(strng) {
+    let splited= strng.split("\n")
+
+    let result= []
+
+    for(let i=0;i<splited.length;i++){
+        result.push(splited[i].split('').reverse().join(""))
+    }
+    console.log(result.join("\n"))
+
+}
+function horMirror(strng) {
+
+    let result = strng.split('\n')
+    console.log(result.reverse().join("\n"))
+}
+function oper(fct, s) {
+    return fct(s)
+}
+
+oper(horMirror, "hSgdHQ\nHnDMao\nClNNxX\niRvxxH\nbqTVvA\nwvSyRu")
