87e47f57f1fc51002ca6fe6b166bca16ed4250aa
pini91 <brenda.loncaric@gmail.com>
2026-07-16 13:36:37 -0400

    Adding new codewars challenge.

---
+++ 7kyu/Automorphic-Number.js
@@ -0,0 +1,24 @@
+// A number is called Automorphic number if and only if its square ends in the same digits as the number itself. For example, 25 is an automorphic number because its square (625) ends with 25.
+
+// Task
+// Given a positive number, determine if it is Automorphic or not. If it is, return "Automorphic", otherwise return "Not!!"
+
+// Examples
+// 25 is an automorphic number, because  25 2 = 625 ends with 25, so return "Automorphic".
+// 13 is not an automorphic number, because  13 2=169 does not end with 13, so return "Not!!".
+// 76 is an automorphic number, because  76 2 =5776 ends with 76, so return "Automorphic".
+// 225 is not an automorphic number, because  225 2 =50625 does not end with 225, so return "Not!!".
+// 625 is an automorphic number, because  625 2 =390625 ends with 625, so return "Automorphic".
+// 1 is an automorphic number, because  1 2 =1 ends with 1, so return "Automorphic".
+// 6 is an automorphic number, because  6 2  =36 ends with 6, so return "Automorphic".
+
+function automorphic(n){
+
+  const square = n**2
+  
+  return String(square).slice( - String(n).length, ) == n? "Automorphic": "Not!!"
+
+return String(square).endsWith(n)?"Automorphic": "Not!!"
+}
+
+console.log(automorphic(1))
